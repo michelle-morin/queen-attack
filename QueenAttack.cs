36659177e48910dee15b812d4c697f51289596d0
Katy Wicz and Michelle Morin <michelle@>
2020-02-24 11:44:36 -0800

    refactor logic to use a queen constructor

--- QueenAttack.cs
+++ QueenAttack.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Collections.Generic;
 
-class QueenAttack
+public class Program
 {
-  static void Main()
+  public static void Main()
   {
     Console.WriteLine("Where is your queen?  Give us her horizontal position");
     string queenStringX = Console.ReadLine();
@@ -10,14 +11,30 @@ class QueenAttack
     string queenStringY = Console.ReadLine();
     int queenPosX = int.Parse(queenStringX);
     int queenPosY = int.Parse(queenStringY);
+    Queen inputQueen = new Queen(queenPosX, queenPosY);
     Console.WriteLine("Where is your opponent?  Give us their horizontal position");
     string oppStringX = Console.ReadLine();
     Console.WriteLine("What is your opponent's vertical position?");
     string oppStringY = Console.ReadLine();
     int oppPosX = int.Parse(oppStringX);
     int oppPosY = int.Parse(oppStringY);
-    int xDiff = queenPosX - oppPosX;
-    int yDiff = queenPosY - oppPosY;
+    inputQueen.CheckAttack(oppPosX, oppPosY);
+  }
+}
+public class Queen
+{
+  public int XPos;
+  public int YPos;
+
+  public Queen(int xPos, int yPos)
+  {
+    XPos = xPos;
+    YPos = yPos;
+  }
+  public void CheckAttack(int oppPosX, int oppPosY)
+  {
+    int xDiff = XPos - oppPosX;
+    int yDiff = YPos - oppPosY;
     if (xDiff == 0 || yDiff ==0 || Math.Abs(xDiff) == Math.Abs(yDiff))
     {
       Console.WriteLine("The Queen can attack her opponent.");
